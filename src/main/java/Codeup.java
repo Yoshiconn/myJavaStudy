6bd68d66b3c833219c29c9203b5e28091ab491af
Yoshiconn <yoshicon0630@gmail.com>
2022-03-20 21:26:30 +0900

    220320

--- src/main/java/Codeup.java
+++ src/main/java/Codeup.java
@@ -1,6 +1,8 @@
-import java.util.Scanner; // 스캐너 클래스 호출
+/*  틀렸던 문제들은 첫 문장 앞에 false 표시
 
-public class Codeup {
+(false) import java.util.Scanner; // 스캐너 클래스 호출
+
+public class Main {
 
     public static void main(String[] args) {
 
@@ -11,3 +13,81 @@ public class Codeup {
     }
 }
 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.Scanner; // 스캐너 클래스 호출
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); // Scanner 라는 객체를 생성한다.
+
+        int a, b; // int 라는 이름의 정수를 담는 변수 a와 b를 생성
+        a = sc.nextInt(); // 정수 값을 입력 받고 a에 저장
+        b = sc.nextInt(); // 정수 값을 입력 받고 b에 저장
+        sc.close(); // 스캐너 사용후 꼭 닫아주기
+
+        System.out.printf("%d %d", a, b); // 입력 받은 값인 a 와 b 를 출력
+    }
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+(false) import java.util.Scanner; // 스캐너 클래스 호출
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); // Scanner 라는 객체를 생성한다.
+
+        char x = sc.next().charAt(0); // 문자 하나를 입력받는 x 라는 char 변수 를 생성한다.
+        char y = sc.next().charAt(0); // 문자 하나를 입력받는 y 라는 char 변수 를 생성한다.
+
+        sc.close(); // 스캐너 사용후 꼭 닫아주기
+
+        System.out.printf("%c %c", y, x); // 입력받은 char 형 변수 y와 x를 출력한다.
+    }
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+(false) import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a;
+        a = sc.next();
+
+        sc.close(); // 스캐너 사용후 꼭 닫아주기
+
+        System.out.printf("%s", a);
+    }
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+(false) import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        sc.close(); // 스캐너 사용후 꼭 닫아주기
+
+        System.out.printf("%s", s);
+    }
+}
+
+
+
+
+*/
---
+++ src/main/java/doitjava.java
@@ -0,0 +1,160 @@
+/*
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 9;
+        int charge = 0;
+
+        if(age < 8){
+            charge = 1000;
+            System.out.println("미 취학 아동입니다.");
+        }
+        else if(age < 14) {
+            charge = 2000;
+            System.out.println("초등학생 입니다.");
+        }
+        else if(age < 20) {
+            charge = 2500;
+            System.out.println("중,고등 학생입니다.");
+        }
+        else {
+            charge = 3000;
+            System.out.println("일반인 입니다.");
+        }
+        System.out.println("입장료는" + charge + "원 입니다.");
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Main {
+    public static void main(String[] args) {
+
+        int month = 10;
+        int day;
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                day = 31;
+                break;
+            case 2:
+                day = 28;
+                break;
+            case 4: case 6: case 9: case 11:
+                day = 30;
+                break;
+            default:
+                day = 0;
+                break;
+        }
+        System.out.println(month + "월은 " + day + "일 까지 있습니다.");
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 1;
+        int sum = 0;
+
+
+        while ( num <= 10) {
+
+            sum += num;
+            num++;
+        }
+        System.out.println("1부터 10까지의 합은 "+ sum + "입니다.");
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 1;
+        int sum = 0;
+
+
+        do {
+            sum += num;
+            num++;
+        }while (num < 1);
+
+        System.out.println("1부터 10까지의 합은 "+ sum + "입니다.");
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int dan;
+        int times;
+
+        for(dan = 2; dan <= 9; dan++) {
+
+            for (times = 1; times<=9; times++) {
+                System.out.println(dan + "X" + times + "=" + dan*times);
+            }
+            System.out.println();
+        }
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int dan = 2;
+        int times = 1;
+
+        while (dan <= 9) {
+            times = 1;
+            while ( times <= 9) {
+                System.out.println(dan + "X" + times + "=" + dan*times);
+                times++;
+            }
+            dan++;
+            System.out.println();
+        }
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Main {
+
+    public static void main(String[] args) {
+        int total = 0;
+        int num;
+
+        for(num = 1; num<=100; num++) {
+
+            if( (num % 2) == 1)
+                continue;
+            total += num;
+        }
+        System.out.println(total);
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+* */
+
+
