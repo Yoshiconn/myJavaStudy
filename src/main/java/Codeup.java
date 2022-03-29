a9c8d4c78559c2c3877e25f036f52f2bdc16bb8f
Yoshiconn <yoshicon0630@gmail.com>
2022-03-29 14:37:02 +0900

    0329코드업 문제풀기

--- src/main/java/Codeup.java
+++ src/main/java/Codeup.java
@@ -113,6 +113,43 @@ public class Main {
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, d;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+
+        int result = a+b+c+d;
+
+        if (result==4) {
+            System.out.println("윷");
+        }
+        else if (result==3){
+            System.out.println("걸");
+        }
+        else if (result==2){
+            System.out.println("개");
+        }
+        else if (result==1){
+            System.out.println("도");
+        }
+        else if (result==0){
+            System.out.println("모");
+        }
+        else {
+            System.out.println("값을 잘못 입력 하였습니다.");
+        }
+    }
+}
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,33 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int a;
-        a = sc.nextInt();
+        int a, b, c, d;
 
-        if(a>0){
-                System.out.println("양수");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+
+        int result = a+b+c+d;
+
+        if (result==4) {
+            System.out.println("윷");
         }
-        else if(a==0){
-            System.out.println("0");
+        else if (result==3){
+            System.out.println("걸");
         }
-        else
-            System.out.println("음수");
+        else if (result==2){
+            System.out.println("개");
+        }
+        else if (result==1){
+            System.out.println("도");
+        }
+        else if (result==0){
+            System.out.println("모");
+        }
+        else {
+            System.out.println("값을 잘못 입력 하였습니다.");
+        }
     }
 }
 
