4a95c36a63677b4eae4b60877e6c81b7ec3a147a
Yoshiconn <yoshicon0630@gmail.com>
2022-04-01 14:34:26 +0900

    220401코드업 문제풀기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,54 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int i, s = 0;
         int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        int cal1, cal2;
-
-        if (a == 1) {
-            cal1 = 400;
-
-        } else if (a == 2) {
-
-            cal1 = 340;
-        } else if (a == 3) {
-
-            cal1 = 170;
-        } else if (a == 4) {
-
-            cal1 = 100;
-        } else {
-
-            cal1 = 70;
+        for(i = 1; i <= a; i++){
+            s+=i;
+            if(s>=a){
+                break;
+            }
         }
-
-        if (b == 1) {
-
-            cal2 = 400;
-        } else if (b == 2) {
-
-            cal2 = 340;
-        } else if (b == 3) {
-
-            cal2 = 170;
-        } else if (b == 4) {
-
-            cal2 = 100;
-        } else {
-
-            cal2 = 70;
-        }
-
-        if ((cal1 + cal2) > 500) {
-
-            System.out.println("angry");
-        } else {
-
-            System.out.println("no angry");
-        }
-
-
+        System.out.println(s);
     }
 }
 
