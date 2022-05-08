947648bc1585c86fa763cdb0d698b2b729d4e587
Yoshiconn <yoshicon0630@gmail.com>
2022-05-09 01:21:35 +0900

    220509 백준 문제 응용

--- src/main/java/back.java
+++ src/main/java/back.java
@@ -5,9 +5,16 @@ public class back {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
         char[] b = a.toCharArray();
-        System.out.print("welcome! ");
-        for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i]);
+        if (b.length > 10) {
+            System.out.println("이름은 10자리를 넘을수 없습니다. \n");
+        } else {
+            System.out.print("welcome! ");
+            for (int i = 0; i < b.length; i++) {
+                System.out.print(b[i]);
+                {
+                    sc.close();
+                }
+            }
         }
     }
 }
