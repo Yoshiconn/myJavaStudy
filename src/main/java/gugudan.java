fa07ddfccd44ee31e98dd20c3a5222f3f4c1094f
Yoshiconn <yoshicon0630@gmail.com>
2022-05-27 19:37:27 +0900

    220512 백준 재귀함수 풀기

--- src/main/java/gugudan.java
+++ src/main/java/gugudan.java
@@ -1,20 +1,29 @@
-import java.util.Scanner;
-
-public class gugudan {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a;
-        int result;
-        int gub = 0; int dan = 0;
-        System.out.print("단을 입력 하세요. : ");
-        a = sc.nextInt();
-
-        for(int i = gub; gub <= i; gub++){
-            for(dan = 1; dan < 10; dan++){
-                System.out.println();
-            }
-        }
-
-    }
-}
+//import java.util.Scanner;
+//
+//public class gugudan {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int a;
+//        int gub = 0;
+//        int dan = 0;
+//        System.out.print("단을 입력 하세요. : ");
+//        a = sc.nextInt();
+//
+//
+//    }
+//
+//    public static int result(int i) {
+//
+//        for (int i1 = 0; i1 < i2; i1++) {
+//
+//        }
+//
+//    }
+//
+//}
+//
+//
+//
+//
+//
