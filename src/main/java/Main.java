22a3524b07ec685d660e5f4cc7c9335029453da5
Yoshiconn <yoshicon0630@gmail.com>
2022-06-27 19:43:15 +0900

    220627 자바공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
+
+    static String underline = "";
+
+    public static void rec(int N) {
+        Scanner sc = new Scanner(System.in);
+        String line = underline;
+
+        if (N == 0) {
+            System.out.println(line + "\"재귀함수가 뭔가요?\"");
+            System.out.println(line + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println(line + "\"라고 답변하였지.\"");
+            return;
+        }
+        System.out.println(line + "\"재귀함수가 뭔가요?\"");
+        System.out.println(line + "\"잘들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\"");
+        System.out.println(line + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        System.out.println(line + "그의 답은 대부분 옳았다고 하네. 그런데 어느날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+
+        underline += "___";
+
+        rec(N-1);
+
+        System.out.println(line + "라고 답변하였지.");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int[] cd = new int[50];
-
-        for(int i = 0; i < n-1; i++){
-            cd[i] = sc.nextInt();
-        }
-
-        Arrays.sort(cd, 0, n-1);
-        for(int i = 0; i < n; i++){
-
-            if(cd[i] != i+1 || cd[i] == 0){
-                System.out.println(i+1);
-                break;
-            }
-        }
+        int N = sc.nextInt();
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+        rec(N);
     }
 }
-
