67ef042dba8ca03cb477c451e4d19d14d7972279
Yoshiconn <yoshicon0630@gmail.com>
2022-06-29 23:57:41 +0900

    220629 자바공부

---
+++ src/main/java/ex_0629.java
@@ -0,0 +1,15 @@
+public class ex_0629 {
+
+    public static void main(String[] args) {
+        int total = 0;
+        int num;
+
+        for(num = 1; num<=100; num++) {
+
+            if( (num % 2) == 1)
+                continue;
+            total += num;
+        }
+        System.out.println(total);
+    }
+}
